058855bb78fe2845e01e403c72fc2e3dd97c1d29
Atd27 <y518a480@gmail.com>
2025-05-26 21:31:27 +0900

    編集処理追加

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-// データ取得
+// データ取得→一覧表示
 Route::get('/todos', [TodoController::class, 'index']);
 
 // データ保存
 Route::post('/todos', [TodoController::class, 'store']);
 
+// データ編集画面
+Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
+
 // データ更新
-Route::put('/todos', [TodoController::class, 'update']);
+Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
 
 // データ削除
-Route::delete('/todos', [TodoController::class, 'destory']);
+Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
