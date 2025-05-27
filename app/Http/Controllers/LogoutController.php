5cc49809fce19b7a44df555be527be33d6c31277
Atd27 <y518a480@gmail.com>
2025-05-27 13:55:07 +0900

    ログアウト処理追加

---
+++ app/Http/Controllers/LogoutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    // ログアウト
+    public function logout(Request $request) {
+        // ユーザーログアウト
+        Auth::logout();
+
+        // セッション無効
+        $request->session()->invalidate();
+
+        // 新しいcsrfトークン再生成
+        $request->session()->regenerateToken();
+
+        // ログインフォームへリダイレクト
+        return redirect('/login');
+    }
+}
