9c9e561877ceab323b2ee2510d9a8c702d8d1f23
Atd27 <y518a480@gmail.com>
2025-05-28 16:39:25 +0900

    新規登録フォーム表示と機能のルート追加

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,10 +22,16 @@ Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update
 Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
 
 // ログインフォーム表示
-Route::get('/login',[LoginController::class, 'showLogin'])->name('showLogin');
+Route::get('/login', [LoginController::class, 'showLogin'])->name('showLogin');
 
 // ログイン処理
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 // ログアウト処理
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+// 新規登録フォーム
+Route::get('/register', [RegisterController::class, 'showRegister'])->name('register.form');
+
+// 新規登録
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
