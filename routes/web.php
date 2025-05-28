9d4d7a10755313be8c8e99169c3f087e3e178df2
Atd27 <y518a480@gmail.com>
2025-05-28 23:52:55 +0900

    ログインしていない場合の/todosへのアクス制御追加

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,11 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-// データ取得→一覧表示
-Route::get('/todos', [TodoController::class, 'index']);
+// 新規登録フォーム
+Route::get('/register', [RegisterController::class, 'showRegister'])->name('register.form');
 
-// データ保存
-Route::post('/todos', [TodoController::class, 'store']);
-
-// データ編集画面
-Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
-
-// データ更新
-Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
-
-// データ削除
-Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
+// 新規登録
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
 
 // ログインフォーム表示
 Route::get('/login', [LoginController::class, 'showLogin'])->name('showLogin');
@@ -30,8 +21,20 @@ Route::post('/login', [LoginController::class, 'login'])->name('login');
 // ログアウト処理
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
-// 新規登録フォーム
-Route::get('/register', [RegisterController::class, 'showRegister'])->name('register.form');
+// ログインしないとできない処理
+Route::middleware(['auth'])->group(function () {
+  // データ取得→一覧表示
+  Route::get('/todos', [TodoController::class, 'index']);
 
-// 新規登録
-Route::post('/register', [RegisterController::class, 'register'])->name('register');
+  // データ保存
+  Route::post('/todos', [TodoController::class, 'store']);
+
+  // データ編集画面
+  Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
+
+  // データ更新
+  Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
+
+  // データ削除
+  Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
+});
