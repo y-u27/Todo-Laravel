aa271f065bbe321fa1198554ac275df6c53bfb16
Atd27 <y518a480@gmail.com>
2025-05-28 16:38:13 +0900

    新規登録のコントローラー作成

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    // 新規登録フォーム表示
+    public function showRegister()
+    {
+        return view('register');
+    }
+
+    // 新規登録
+    public function register(Request $request)
+    {
+        // リクエストデータ検証
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
+        // ユーザー作成
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        // 自動ログイン
+        Auth::login($user);
+        
+        return redirect('/todos');
+    }
+}
